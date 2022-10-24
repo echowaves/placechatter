7b9d7e6dc4503ee44862f13d6abc3f43c15ff562
dmitryame <dmitry@custommode.com>
2022-10-23 22:00:50 -0400

    wip: adding photo back

--- src/screens/Place/placeCardEdit.js
+++ src/screens/Place/placeCardEdit.js
@@ -65,219 +65,212 @@ function PlaceCardEdit({ route, navigation }) {
   const [cardTitle, setCardTitle] = useState('')
   const [cardText, setCardText] = useState('')
 
+  const [cardPhoto, setCardPhoto] = useState()
+
   const [cardTitleError, setCardTitleError] = useState('')
   const [cardTextError, setCardTextError] = useState('')
 
-  // const uploadImage = async ({ placeUuid, contentType, assetUri }) => {
-  //   const photoUuid = uuidv4()
-  //   const { uuid, phoneNumber, token } = authContext
+  const uploadImage = async ({ placeUuid, contentType, assetUri }) => {
+    const photoUuid = uuidv4()
+    const { uuid, phoneNumber, token } = authContext
 
-  //   const photoForUpload = (
-  //     await CONST.gqlClient.mutate({
-  //       mutation: gql`
-  //         mutation generateUploadUrl(
-  //           $uuid: String!
-  //           $phoneNumber: String!
-  //           $token: String!
-  //           $assetKey: String!
-  //           $contentType: String!
-  //           $placeUuid: String
-  //         ) {
-  //           generateUploadUrl(
-  //             uuid: $uuid
-  //             phoneNumber: $phoneNumber
-  //             token: $token
-  //             assetKey: $assetKey
-  //             contentType: $contentType
-  //             placeUuid: $placeUuid
-  //           ) {
-  //             photo {
-  //               thumbUrl
-  //               photoUuid
-  //             }
-  //             uploadUrl
-  //           }
-  //         }
-  //       `,
-  //       variables: {
-  //         uuid,
-  //         phoneNumber,
-  //         token,
-  //         assetKey: photoUuid,
-  //         contentType,
-  //         placeUuid,
-  //       },
-  //     })
-  //   ).data.generateUploadUrl
+    const photoForUpload = (
+      await CONST.gqlClient.mutate({
+        mutation: gql`
+          mutation generateUploadUrl(
+            $uuid: String!
+            $phoneNumber: String!
+            $token: String!
+            $assetKey: String!
+            $contentType: String!
+            $placeUuid: String
+          ) {
+            generateUploadUrl(
+              uuid: $uuid
+              phoneNumber: $phoneNumber
+              token: $token
+              assetKey: $assetKey
+              contentType: $contentType
+              placeUuid: $placeUuid
+            ) {
+              photo {
+                thumbUrl
+                photoUuid
+              }
+              uploadUrl
+            }
+          }
+        `,
+        variables: {
+          uuid,
+          phoneNumber,
+          token,
+          assetKey: photoUuid,
+          contentType,
+          placeUuid,
+        },
+      })
+    ).data.generateUploadUrl
 
-  //   // console.log({ photoForUpload })
-  //   // console.log({ assetUri })
-  //   // CacheManager.addToCache({
-  //   //   file: assetUri,
-  //   //   key: `${photoUuid}`,
-  //   // })
-  //   // CacheManager.addToCache({
-  //   //   file: assetUri,
-  //   //   key: `${photoUuid}-thumb`,
-  //   // })
+    // console.log({ photoForUpload })
+    // console.log({ assetUri })
+    // CacheManager.addToCache({
+    //   file: assetUri,
+    //   key: `${photoUuid}`,
+    // })
+    // CacheManager.addToCache({
+    //   file: assetUri,
+    //   key: `${photoUuid}-thumb`,
+    // })
 
-  //   const { uploadUrl, photo } = photoForUpload
+    const { uploadUrl, photo } = photoForUpload
 
-  //   const responseData = await FileSystem.uploadAsync(uploadUrl, assetUri, {
-  //     httpMethod: 'PUT',
-  //     headers: {
-  //       'Content-Type': contentType,
-  //     },
-  //   })
-  //   // console.log({ responseData })
-  //   return { responseData, photo }
-  // }
+    const responseData = await FileSystem.uploadAsync(uploadUrl, assetUri, {
+      httpMethod: 'PUT',
+      headers: {
+        'Content-Type': contentType,
+      },
+    })
+    // console.log({ responseData })
+    return { responseData, photo }
+  }
 
-  // const takePhoto = async () => {
-  //   if (photos.length > 10) {
-  //     Toast.show({
-  //       text1: 'More than 10 photos.',
-  //       text2: 'Delete some photos first',
-  //       type: 'info',
-  //       topOffset,
-  //     })
-  //     return
-  //   }
-  //   // launch photo capturing
-  //   const cameraPermission = await ImagePicker.requestCameraPermissionsAsync()
-  //   // console.log({ cameraPermission })
+  const takePhoto = async () => {
+    // launch photo capturing
+    const cameraPermission = await ImagePicker.requestCameraPermissionsAsync()
+    // console.log({ cameraPermission })
 
-  //   if (!cameraPermission?.granted) {
-  //     Alert.alert(
-  //       'Photo permission is not granted',
-  //       'Allow photo taking in settings',
-  //       [
-  //         {
-  //           text: 'Open Settings',
-  //           onPress: () => {
-  //             Linking.openSettings()
-  //           },
-  //         },
-  //       ],
-  //     )
-  //     return
-  //   }
-  //   const mediaPermission =
-  //     await ImagePicker.requestMediaLibraryPermissionsAsync()
-  //   if (!mediaPermission?.granted) {
-  //     Alert.alert(
-  //       'Photo library permission is not granted',
-  //       'Allow photos to be saved  on your device in settings',
-  //       [
-  //         {
-  //           text: 'Open Settings',
-  //           onPress: () => {
-  //             Linking.openSettings()
-  //           },
-  //         },
-  //       ],
-  //     )
-  //     return
-  //   }
+    if (!cameraPermission?.granted) {
+      Alert.alert(
+        'Photo permission is not granted',
+        'Allow photo taking in settings',
+        [
+          {
+            text: 'Open Settings',
+            onPress: () => {
+              Linking.openSettings()
+            },
+          },
+        ],
+      )
+      return
+    }
+    const mediaPermission =
+      await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (!mediaPermission?.granted) {
+      Alert.alert(
+        'Photo library permission is not granted',
+        'Allow photos to be saved  on your device in settings',
+        [
+          {
+            text: 'Open Settings',
+            onPress: () => {
+              Linking.openSettings()
+            },
+          },
+        ],
+      )
+      return
+    }
 
-  //   const cameraReturn = await ImagePicker.launchCameraAsync({
-  //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
-  //     quality: 1.0,
-  //     // allowsEditing: true,
-  //     // aspect: [1, 1],
-  //     exif: false,
-  //   })
+    const cameraReturn = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1.0,
+      // allowsEditing: true,
+      // aspect: [1, 1],
+      exif: false,
+    })
 
-  //   // alert(`cameraReturn.cancelled ${cameraReturn.cancelled}`)
-  //   if (cameraReturn.cancelled === false) {
-  //     setShowSpinner(true)
+    // alert(`cameraReturn.cancelled ${cameraReturn.cancelled}`)
+    if (cameraReturn.cancelled === false) {
+      setShowSpinner(true)
 
-  //     // have to wait, otherwise the upload will not start
-  //     // await dispatch(
-  //     //   reducer.queueFileForUpload({
-  //     //     cameraImgUrl: cameraReturn.uri,
-  //     //     type: cameraReturn.type,
-  //     //     location,
-  //     //   }),
-  //     // )
-  //     // dispatch(reducer.uploadPendingPhotos())
-  //     // console.log({ cameraReturn })
-  //     const imageWidth = cameraReturn.width
-  //     const imageHeight = cameraReturn.height
-  //     const imageCropDim = imageWidth < imageHeight ? imageWidth : imageHeight
+      // have to wait, otherwise the upload will not start
+      // await dispatch(
+      //   reducer.queueFileForUpload({
+      //     cameraImgUrl: cameraReturn.uri,
+      //     type: cameraReturn.type,
+      //     location,
+      //   }),
+      // )
+      // dispatch(reducer.uploadPendingPhotos())
+      // console.log({ cameraReturn })
+      const imageWidth = cameraReturn.width
+      const imageHeight = cameraReturn.height
+      const imageCropDim = imageWidth < imageHeight ? imageWidth : imageHeight
 
-  //     const croppedImage = await ImageManipulator.manipulateAsync(
-  //       cameraReturn.uri,
-  //       [
-  //         {
-  //           crop: {
-  //             originX: (imageWidth - imageCropDim) / 2,
-  //             originY: (imageHeight - imageCropDim) / 2,
-  //             height: imageCropDim,
-  //             width: imageCropDim,
-  //           },
-  //         },
-  //       ],
-  //       { compress: 1, format: ImageManipulator.SaveFormat.PNG },
-  //     )
+      const croppedImage = await ImageManipulator.manipulateAsync(
+        cameraReturn.uri,
+        [
+          {
+            crop: {
+              originX: (imageWidth - imageCropDim) / 2,
+              originY: (imageHeight - imageCropDim) / 2,
+              height: imageCropDim,
+              width: imageCropDim,
+            },
+          },
+        ],
+        { compress: 1, format: ImageManipulator.SaveFormat.PNG },
+      )
 
-  //     MediaLibrary.saveToLibraryAsync(croppedImage.uri)
+      MediaLibrary.saveToLibraryAsync(croppedImage.uri)
 
-  //     const locallyThumbedImage = await ImageManipulator.manipulateAsync(
-  //       croppedImage.uri,
-  //       [{ resize: { height: 300 } }],
-  //       { compress: 1, format: ImageManipulator.SaveFormat.PNG },
-  //     )
+      const locallyThumbedImage = await ImageManipulator.manipulateAsync(
+        croppedImage.uri,
+        [{ resize: { height: 300 } }],
+        { compress: 1, format: ImageManipulator.SaveFormat.PNG },
+      )
 
-  //     // const manipResult = await ImageManipulator.manipulateAsync(
-  //     //   localImgUrl,
-  //     //   [{ resize: { height: 300 } }],
-  //     //   { compress: 1, format: ImageManipulator.SaveFormat.PNG },
-  //     // )
-  //     // return manipResult.uri
-  //     // console.log('cancelled not')
-  //     try {
-  //       const response = await uploadImage({
-  //         placeUuid: placeContext?.place?.placeUuid,
-  //         contentType: 'image/png',
-  //         assetUri: croppedImage.uri,
-  //       })
-  //       // console.log({ response })
+      // const manipResult = await ImageManipulator.manipulateAsync(
+      //   localImgUrl,
+      //   [{ resize: { height: 300 } }],
+      //   { compress: 1, format: ImageManipulator.SaveFormat.PNG },
+      // )
+      // return manipResult.uri
+      // console.log('cancelled not')
+      try {
+        const response = await uploadImage({
+          placeUuid: placeContext?.place?.placeUuid,
+          contentType: 'image/png',
+          assetUri: croppedImage.uri,
+        })
+        // console.log({ response })
 
-  //       await CacheManager.addToCache({
-  //         file: croppedImage.uri,
-  //         key: `${response.photo.photoUuid}`,
-  //       })
-  //       await CacheManager.addToCache({
-  //         file: locallyThumbedImage.uri,
-  //         key: `${response.photo.photoUuid}-thumb`,
-  //       })
+        await CacheManager.addToCache({
+          file: croppedImage.uri,
+          key: `${response.photo.photoUuid}`,
+        })
+        await CacheManager.addToCache({
+          file: locallyThumbedImage.uri,
+          key: `${response.photo.photoUuid}-thumb`,
+        })
 
-  //       // const photosClone = photos
-  //       // await setPhotos([])
-  //       // await setPhotos([response.photo, ...photosClone])
-  //       setPlaceContext({
-  //         ...placeContext,
-  //         cards: [response.photo, ...photos],
-  //       })
-  //     } catch (err10) {
-  //       console.log({ err10 })
-  //       Toast.show({
-  //         text1: 'Unable to add photo',
-  //         text2: err10.toString(),
-  //         type: 'error',
-  //         topOffset,
-  //       })
-  //     }
-  //     // console.log({ responseData: response.responseData })
-  //     // init()
-  //     // console.log(reloadedPlace.photos[0])
-  //     // setShowSpinner(false)
-  //     // console.log({ reloadedPlace })
+        // const photosClone = photos
+        // await setPhotos([])
+        // await setPhotos([response.photo, ...photosClone])
+        // setPlaceContext({
+        //   ...placeContext,
+        //   cards: [response.photo, ...photos],
+        // })
+      } catch (err10) {
+        console.log({ err10 })
+        Toast.show({
+          text1: 'Unable to add photo',
+          text2: err10.toString(),
+          type: 'error',
+          topOffset,
+        })
+      }
+      // console.log({ responseData: response.responseData })
+      // init()
+      // console.log(reloadedPlace.photos[0])
+      // setShowSpinner(false)
+      // console.log({ reloadedPlace })
 
-  //     setShowSpinner(false)
-  //   }
-  // }
+      setShowSpinner(false)
+    }
+  }
 
   const exit = async () => {
     await navigation.popToTop()
@@ -307,7 +300,7 @@ function PlaceCardEdit({ route, navigation }) {
       size={30}
       style={{
         marginLeft: 10,
-        color: CONST.MAIN_COLOR,
+        color: CONST.SECONDARY_COLOR,
         width: 60,
       }}
       onPress={() => {
@@ -384,6 +377,7 @@ function PlaceCardEdit({ route, navigation }) {
       // console.log({ place })
       setCardTitle(loadedCard?.cardTitle)
       setCardText(loadedCard?.cardText)
+      setCardPhoto(loadedCard?.photo)
 
       navigation.setOptions({
         headerTitle: loadedCard?.cardTitle,
@@ -531,6 +525,14 @@ function PlaceCardEdit({ route, navigation }) {
             autoCorrect={true}
             // autoFocus={true}
           />
+
+          {!cardPhoto && (
+            <Button onPress={takePhoto} size="lg" iconRight color="green">
+              {`  Add Photo`}
+              <Icon name="camera" color="white" />
+            </Button>
+          )}
+          <Card.Divider />
           <Input
             label="Card Text"
             // leftIcon={{ type: 'MaterialIcons', name: 'description' }}
