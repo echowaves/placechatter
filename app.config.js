9c374cbb9b3c85c8036c79071a6a8b805eb40132
dmitryame <dmitry@custommode.com>
2022-12-08 18:36:55 -0500

    build 27

--- app.config.js
+++ app.config.js
@@ -41,7 +41,7 @@ export default {
     assetBundlePatterns: ['**/*'],
     ios: {
       bundleIdentifier: 'com.echowaves.placechatter',
-      buildNumber: '26',
+      buildNumber: '27',
       supportsTablet: false,
       associatedDomains: ['applinks:link.placechatter.com'],
       config: {
@@ -74,7 +74,7 @@ export default {
         backgroundColor: '#FFFFFF',
       },
       package: 'com.echowaves.placechatter',
-      versionCode: 26,
+      versionCode: 27,
       permissions: [
         'INTERNET',
         'SYSTEM_ALERT_WINDOW',
